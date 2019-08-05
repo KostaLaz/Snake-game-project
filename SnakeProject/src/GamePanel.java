99323641df26cae3f4e69a79883f3e1d235745b4
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-08-05 17:46:18 +0200

    Commit

--- SnakeProject/src/GamePanel.java
+++ SnakeProject/src/GamePanel.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
 
@@ -11,15 +13,19 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	public static final int WIDTH = 500, HEIGHT = 500;
 
-	public Thread thread;
+	private Thread thread;
 	
-	public boolean running;
+	private boolean running;
 	
+	private BodyPart b;
+	private ArrayList<BodyPart> snake;
+	private int xColor = 10, yColor = 10, size = 5;
+	private int ticks = 0;
 	
 	public GamePanel() {
 		
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		
+		snake = new ArrayList<BodyPart>();
 	}
 	
 	public void start() {
@@ -39,7 +45,11 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void tick() {
-		
+		if(snake.size() == 0) {
+			b = new BodyPart(xColor, yColor, 10);
+			snake.add(b);
+		}
+		ticks++;
 	}
 	
 	public void paint(Graphics g) {
@@ -53,7 +63,9 @@ public class GamePanel extends JPanel implements Runnable{
 		for(int i = 0; i<HEIGHT/10; i++) {
 			g.drawLine( 0, i * 10, HEIGHT, i * 10 );
 		}
-		
+		for(int i = 0; i< snake.size(); i++) {
+			snake.get(i).draw(g);
+		}
 	}
 
 	@Override
