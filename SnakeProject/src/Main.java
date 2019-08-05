04bbd5889c7fa03c99f3c2c2b85d3831b56a6983
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-08-05 16:52:47 +0200

    Second commit

--- SnakeProject/src/Main.java
+++ SnakeProject/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public Main() {
 		
 		JFrame frame = new JFrame();
+		GamePanel gamepanel = new GamePanel();
 		
+		frame.add(gamepanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("SNAKE");
 		frame.setLocationRelativeTo(null);
